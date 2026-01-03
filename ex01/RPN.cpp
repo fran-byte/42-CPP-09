1a88fdeb761acc838116544be7db4f2f1232fc75
fran-byte <fran-byte@hotmail.com>
2026-01-03 19:38:34 +0100

    up

---
+++ ex01/RPN.cpp
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/03 12:34:42 by frromero          #+#    #+#             */
+/*   Updated: 2026/01/03 19:34:02 by frromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "RPN.hpp"
+#include <exception>
+#include <stdexcept>
+#include <cctype>
+
+RPN::RPN(void) {}
+
+RPN::RPN(const std::string &str)
+{
+    double a = 0;
+    double b = 0;
+    for (size_t i = 0; i < str.length(); i++)
+    {
+        char c = str[i];
+
+        if (c == ' ' || c == '\t')
+            continue;
+
+        e_token token = validateTokens(c);
+
+        switch (token)
+        {
+        case INT:
+            _rpnStack.push(static_cast<float>(c - '0'));
+            break;
+
+        case ADD:
+            if (_rpnStack.size() >= 2)
+            {
+                b = _rpnStack.top();
+                _rpnStack.pop();
+                a = _rpnStack.top();
+                _rpnStack.pop();
+                _rpnStack.push(a + b);
+            }
+            else
+                throw std::runtime_error("Error");
+            break;
+
+        case SUBT:
+            if (_rpnStack.size() >= 2)
+            {
+                b = _rpnStack.top();
+                _rpnStack.pop();
+                a = _rpnStack.top();
+                _rpnStack.pop();
+                _rpnStack.push(a - b);
+            }
+            else
+                throw std::runtime_error("Error");
+            break;
+
+        case MULT:
+            if (_rpnStack.size() >= 2)
+            {
+                b = _rpnStack.top();
+                _rpnStack.pop();
+                a = _rpnStack.top();
+                _rpnStack.pop();
+                _rpnStack.push(a * b);
+            }
+            else
+                throw std::runtime_error("Error");
+            break;
+
+        case DIV:
+            if (_rpnStack.size() >= 2)
+            {
+                b = _rpnStack.top();
+                _rpnStack.pop();
+                if (b == 0)
+                    throw std::runtime_error("Error");
+                a = _rpnStack.top();
+                _rpnStack.pop();
+                _rpnStack.push(a / b);
+            }
+            else
+                throw std::runtime_error("Error");
+            break;
+
+        case ERROR:
+        default:
+            throw std::runtime_error("Error");
+        }
+    }
+}
+
+RPN::RPN(RPN const &copy) : _rpnStack(copy._rpnStack) {}
+
+RPN::~RPN(void) {}
+
+RPN &RPN::operator=(RPN const &other)
+{
+    if (this != &other)
+        _rpnStack = other._rpnStack;
+    return *this;
+}
+
+float RPN::calculateRPN()
+{
+    //
+}
+
+e_token validateTokens(char c)
+{
+    //
+}
---
+++ ex01/RPN.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/03 12:05:42 by frromero          #+#    #+#             */
+/*   Updated: 2026/01/03 19:18:08 by frromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RPN_HPP
+#define RPN_HPP
+#include <stack>
+#include <string>
+
+enum e_token
+{
+    INT,
+    ADD,
+    SUBT,
+    MULT,
+    DIV,
+    ERROR
+};
+
+class RPN
+{
+private:
+    std::stack<float> _rpnStack;
+    e_token validateTokens(char c);
+
+public:
+    RPN(void);
+    RPN(const std::string &str);
+    RPN(RPN const &copy);
+    ~RPN(void);
+    RPN &operator=(RPN const &other);
+    float calculateRPN();
+};
+
+#endif
